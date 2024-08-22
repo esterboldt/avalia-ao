26875ce93aed8bf3c3a6998aa141a803bbed3814
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-08-22 11:03:01 -0300

    2208ar

---
+++ Models/Message.cs
@@ -0,0 +1,9 @@
+namespace projetinho.Models
+{
+    public class Message
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Content { get; set; }
+    }
+}
